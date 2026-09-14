f45bd44ced7a5ba1c16bccf50c1af5cc22efbd44
RubberDuckBug <cabbagecode@gmail.com>
2026-09-14 17:53:14 +0100

    Started work on the cv page

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/cv')
+def cv():
+    return render_template('cv.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
