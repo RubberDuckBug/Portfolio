18d3a62382b01eadea5df427faa3393f63a96c2e
RubberDuckBug <cabbagecode@gmail.com>
2026-09-20 19:22:40 +0100

    AWS fixing beanstalk

--- app/config.py
+++ app/config.py
@@ -5,4 +5,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
